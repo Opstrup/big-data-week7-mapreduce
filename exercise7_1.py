c21b0d8c135211686f5d3f331a11bb6215b6b289
Anders Opstrup <anders@coffeecode.dk>
2017-10-24 14:17:41 +0200

    finished exercise 1 correctly and added small test file

--- exercise7_1.py
+++ exercise7_1.py
@@ -1,12 +1,26 @@
 from mrjob.job import MRJob
+from mrjob.job import MRStep
+import re
+
+WORD = re.compile(r"[\w']+")
 
 class wordCounter(MRJob):
 
-    def mapper(self, _, line):
-        yield "words", len(line.split())
+    def steps(self):
+        return [
+                MRStep(mapper=self.mapper_get_words,
+                       reducer=self.reducer_counted_words)
+                ]
 
-    def reducer(self, key, values):
-        yield key, sum(values)
+    def mapper_get_words(self, _, line):
+        for word in WORD.findall(line):
+            yield (word.lower(), 1)
+
+    def combine_count_word(self, word, counts):
+        yield (word, sum(counts))
+
+    def reducer_counted_words(self, word, counts):
+        yield (word, sum(counts))
 
 if __name__ == '__main__':
     wordCounter.run()
