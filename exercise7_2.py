f9d717f32d7e81ad04e7cd8a656bd7870a9b74e6
Anders Opstrup <anders@coffeecode.dk>
2017-10-26 20:25:54 +0200

    improved euler checker

--- exercise7_2.py
+++ exercise7_2.py
@@ -3,28 +3,25 @@ from mrjob.step import MRStep
 
 class EulerChecker(MRJob):
 
-    def mapper_node_connections(self, _, line):
-        yield ((line[0], line[2]), 1)
+    def mapper_node_connections(self, _, edge):
+        yield (edge.split()[0]), 1
+        yield (edge.split()[1]), 1
 
-    def combiner_count_connections(self, connection, counts):
-        yield (connection, sum(counts))
+    def reducer_sum_connections(self, _, counts):
+        yield None, sum(counts)
 
-    def reducer_check_for_euler(self, _, value):
-        if (1 != next(value)):
-            yield ("Not an euler", 1)
-        else:
-            yield ("An euler", 1)
-
-    def combiner_check_not_euler_appereance(self, key, values):
-        yield (key, sum(values))
+    def reducer_check_for_euler(self, _, degrees):
+        has_euler_tour = True
+        for degree in degrees:
+            has_euler_tour = has_euler_tour & (degree % 2 == 0) 
+        yield "Has euler tour", has_euler_tour
 
     def steps(self):
         return [
             MRStep(mapper=self.mapper_node_connections,
-                   combiner=self.combiner_count_connections,
-                   reducer=self.reducer_check_for_euler
+                   reducer=self.reducer_sum_connections
                    ),
-            MRStep(reducer=self.combiner_check_not_euler_appereance)
+            MRStep(reducer=self.reducer_check_for_euler)
         ]
 
 if __name__ == '__main__':
